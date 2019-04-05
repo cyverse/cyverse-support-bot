21f229361096641f69fb482a01a46f0b3ebea89a
Calvin McLean <calvinmclean@email.arizona.edu>
2019-04-05 13:25:16 -0700

    Fix error in 'get_next_day'

--- bot.py
+++ bot.py
@@ -189,7 +189,7 @@ class AtmoSupportBot:
         """
         logging.info("Getting day from calendar for user %s" % name)
         days = filter(lambda e: name.lower() in e['summary'].lower() and "Atmosphere Support" in e['summary'], self.get_event_list())
-        return dt.strptime(days[0].get('date'), "%Y-%m-%d").strftime("%A %Y-%m-%d") if days else "not on the calendar"
+        return dt.strptime(days[0]['start'].get('date'), "%Y-%m-%d").strftime("%A %Y-%m-%d") if days else "not on the calendar"
 
 
     def next_seven_days(self):
